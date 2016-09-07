dd775ac3a613e56eaeed4f17b46f1ef909ce3860
Michael Q <MichaelQuiban@outlook.com>
2016-09-06 23:03:42 -0700

    Psuedo Mindmap added to Javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+$(document).ready(function(){
+
+
+//Each contact should be stored as an object with a number of properties
+
+//Submitting a form should create a new object with whatever properties submitted.
+
+//Users can't submit without a minimum amount of information
+
+
+});
