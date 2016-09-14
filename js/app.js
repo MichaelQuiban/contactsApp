1609b2a67fc140ad4027ebb4981e2ff147a27795
Michael Q <MichaelQuiban@outlook.com>
2016-09-13 21:37:19 -0700

    Added names for Jquery to serialize data.
    Added function that creates an object from the form data!

--- js/app.js
+++ js/app.js
@@ -7,7 +7,16 @@ $(document).ready(function(){
 //3. First Name and Last Name are placed under 'Contacts'
 //4. User clicks First Name and Last name and displays within 'info-show'
 
-var form = document.getElementById("contact-form");
+	//The contact information will go here.
+	var contacts = [];
+
+	//Create object from form creation.
+	$("#contact-form").submit(function(event) {
+		console.log( $(this).serializeArray() );
+		event.preventDefault();
+	});
+
+	
 
 
 });
