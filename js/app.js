21614ee25725c02b7f9523ccf5ea52002b1de9d3
Michael Q <MichaelQuiban@outlook.com>
2016-09-18 08:48:23 -0700

    Added ability to reset form!

--- js/app.js
+++ js/app.js
@@ -1,30 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+    //Function that creates an object and display it.
+    function showContact() {
+        // Serialize and create an object out of form data.
+        var forms = $("#contact-form").serializeArray();
+        // Empty the contact info when we're done serializing.
+        $("#contact-info").empty();
 
+        // Append the form data that was serialized.
+        $("#contact-info").append("<a>" + forms.value + "</a>" + " ");
 
-//1. Store Contacts as an object
-//2. Submitting a form creates an object.
-//3. First Name and Last Name are placed under 'Contacts'
-//4. User clicks First Name and Last name and displays within 'info-show'
-
-	//Function that creates an object and display it.
-	function showContact() {
-			// Serialize and create an object out of form data.
-		var forms = $("#contact-form").serializeArray();
-			// Empty the contact info when we're done serializing.
-		$("#contact-info").empty();
-			// Nested function to handle the serialized data and append it.
-		$.each( forms, function ( i, forms ) {
-			// Append the form data that was serialized.
-			$("#contact-info").append( "<a>" + forms.value + "</a>" + " ");
-		});
-	}
-	//Perform the showContact function when submit is clicked.
-	$("#submit").click(function() {
-		$("#submit").click(showContact);
-		event.preventDefault();
-	});
-
-	showContact();
+        // Reset Form Fields
+    }
+    
+    //Perform the showContact function when submit is clicked.
+    $("#submit").click(function() {
+        event.preventDefault();
+        $("#submit").click(showContact);
+        $(this).closest("#form-group").find("input[type=text]".val("");
+      	
+    });
+    showContact();
 
 });
